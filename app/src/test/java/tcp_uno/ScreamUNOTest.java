b22c4146d8145ad871be0337dafad03748fc9f93
Diego Cardoso Nunes <dcnunes@inf.ufrgs.br>
2023-07-27 15:51:21 +0000

    ScreamUNO action and tests

---
+++ app/src/test/java/tcp_uno/ScreamUNOTest.java
@@ -0,0 +1,31 @@
+package tcp_uno;
+
+import org.junit.Before;
+import org.junit.Test;
+import tcp_uno.game.*;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class ScreamUNOTest {
+    private GameBoard gameBoard;
+    private Player player;
+
+    private final Card yellow4 = new Card(CardColor.YELLOW, CardValue.NUM_4);
+    private final Card yellow5 = new Card(CardColor.YELLOW, CardValue.NUM_5);
+
+    @Before
+    public void setUp() {
+        this.gameBoard = new GameBoard(4);
+        this.gameBoard.addToDiscardPile(yellow4);
+        this.player = new Player(List.of(yellow5));
+    }
+
+    @Test
+    public void testScreamUno() {
+        ScreamUNO screamUNO = new ScreamUNO(player, gameBoard);
+        screamUNO.execute();
+        assertTrue(player.saidUno());
+    }
+}
