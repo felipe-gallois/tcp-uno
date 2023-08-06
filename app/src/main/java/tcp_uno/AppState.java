630d208f84ce0bf6e613d9e961df76d485163a0d
Vinicius J. F <www.vini84200@hotmail.com>
2023-08-06 10:38:27 -0300

    Created Application View and AppState

---
+++ app/src/main/java/tcp_uno/AppState.java
@@ -0,0 +1,23 @@
+package tcp_uno;
+
+import tcp_uno.view.View;
+
+public enum AppState {
+    MENU,
+    PLAYING,
+    CREDITS,
+    EXIT;
+
+    View getView() {
+        switch (this) {
+            case MENU:
+//                return new MenuView();
+            case PLAYING:
+//                return new GameView();
+            case CREDITS:
+//                return new CreditsView();
+            default:
+                return null;
+        }
+    }
+}
--- app/src/main/java/tcp_uno/Application.java
+++ app/src/main/java/tcp_uno/Application.java
@@ -1,23 +1,56 @@
 package tcp_uno;
 
-import static com.raylib.Jaylib.LIGHTGRAY;
-import static com.raylib.Jaylib.RAYWHITE;
+import tcp_uno.view.View;
+
 import static com.raylib.Raylib.*;
 
 public class Application {
-    public void run() {
-        InitWindow(800, 450, "Demo");
-        SetTargetFPS(60);
-        while (!WindowShouldClose()) {
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
-            EndDrawing();
-        }
+    public static final String WINDOW_TITLE = "Uno Game";
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 450;
+    private AppState appState;
+    private View activeView;
+
+    Application() {
+        appState = AppState.MENU;
+//        activeView = new MenuView();
+    }
+
+    private static void closeWindow() {
         CloseWindow();
     }
+
+    private static void initWindow() {
+        InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+        SetTargetFPS(60);
+    }
+
     public static void main(String[] args) {
         Application app = new Application();
         app.run();
+    }
+
+    public void run() {
+        initWindow();
+
+        while (shouldKeepRunning()) {
+            AppState newAppState = activeView.update();
+            activeView.display();
+            if (newAppState != appState) {
+                onStateChange(newAppState);
+            }
+        }
+
+        closeWindow();
+    }
+
+    private boolean shouldKeepRunning() {
+        return !WindowShouldClose()
+                && appState != AppState.EXIT;
+    }
+
+    public void onStateChange(AppState newState) {
+        appState = newState;
+        activeView = appState.getView();
     }
 }
